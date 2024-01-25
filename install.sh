e71c033e666e52b99b3a1cc3269bb02075d1ba21
PJ Evans <pj.evans@mindera.com>
2024-01-25 12:55:12 +0000

    Install script WIP

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 # Updates & Depedencies
 sudo apt -y update && sudo apt -y upgrade
-sudo apt install git vlc
+sudo apt -y install git vlc
 
 # Repo checkout
 rm -rf ~/playbackos_repo
 git clone https://github.com/mrpjevans/playbackos.git ~/playbackos_repo
 
 # VLC Config
+mkdir ~/.config/vlc
 cp ~/playbackos_repo/assets/vlcrc ~/.config/vlc/
 
 # Directory setup
@@ -20,6 +21,6 @@ echo ../videos/playback_os_ident_1.mp4 > ~/playbackos/playlists/boot.m3u
 # Autostart
 cat >> ~/.config/wayfire.ini << EOF
 [autostart]
-vlc = cvlc $(pwd)/playbackos/playlists/boot.m3u
+vlc = cvlc $HOME/playbackos/playlists/boot.m3u
 EOF
 
