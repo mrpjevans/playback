08522f8d362a3964497acc149afd46b259faa1a4
PJ Evans <pj.evans@mindera.com>
2024-01-25 13:48:27 +0000

    Desktop setup

--- install.sh
+++ install.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+WAYFIRE_FILE="$HOME/.config/wayfire.ini"
+
 # Updates & Depedencies
 sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install git vlc
+
+
+# Repo checkout
+rm -rf $HOME/playbackos_repo
+git clone https://github.com/mrpjevans/playbackos.git $HOME/playbackos_repo
+
+# VLC Config
+mkdir $HOME/.config/vlc
+cp $HOME/playbackos_repo/assets/vlcrc $HOME/.config/vlc/
+
+# Directory setup
+rm -rf $HOME/playbackos
+mkdir -p $HOME/playbackos/videos $HOME/playbackos/playlists
+cp $HOME/playbackos_repo/assets/playback_os_ident_1.mp4 $HOME/playbackos/videos/
+echo ../videos/playback_os_ident_1.mp4 > $HOME/playbackos/playlists/boot.m3u
+
+# Autostart
+cat >> $WAYFIRE_FILE << EOF
+[autostart]
+vlc = cvlc $HOMEDIR/playbackos/playlists/boot.m3u
+EOF
+
+# Set up desktop
+mkdir -p $HOME/pcmanfm/LXDE-pi
+cp $HOME/playbackos_repo/assets/desktop-items-o.conf $HOME/pcmanfm/LXDE-pi/
 
 # Set login to auto/desktop
 sudo systemctl --quiet set-default graphical.target
@@ -14,23 +41,7 @@ ExecStart=
 ExecStart=-/sbin/agetty --autologin $USER --noclear %I \$TERM
 EOF"
 
-# Repo checkout
-rm -rf ~/playbackos_repo
-git clone https://github.com/mrpjevans/playbackos.git ~/playbackos_repo
-
-# VLC Config
-mkdir ~/.config/vlc
-cp ~/playbackos_repo/assets/vlcrc ~/.config/vlc/
-
-# Directory setup
-rm -rf ~/playbackos
-mkdir -p ~/playbackos/videos ~/playbackos/playlists
-cp ~/playbackos_repo/assets/playback_os_ident_1.mp4 ~/playbackos/videos/
-echo ../videos/playback_os_ident_1.mp4 > ~/playbackos/playlists/boot.m3u
-
-# Autostart
-cat >> ~/.config/wayfire.ini << EOF
-[autostart]
-vlc = cvlc $HOME/playbackos/playlists/boot.m3u
-EOF
-
+# Disable screen blanking
+if grep -q dpms_timeout $WAYFIRE_FILE ; then
+    sed -i 's/dpms_timeout.*/dpms_timeout=600/' $WAYFIRE_FILE
+fi
